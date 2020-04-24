9fb32f26c4ecabb7d6168a0d2dd29c17b3c8bbd7
Вячеслав Шахтаров <45519709+000mrLuigi000@users.noreply.github.com>
2020-04-24 23:22:43 +0300

    Revert "Выполнил задание урока 0"
    This reverts commit 3ddb04dbb2ef40c2d1b7c010b8e839a537c95518.

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'); //добавляю модуль
-var app = express(); //создаю обьект от модуля
-
-app.use(express.static('scr')); //устанавливаю рабочую папку
-
-app.listen(3000, function(){  //запускает сервер 
-    console.log('It works!');
-});
